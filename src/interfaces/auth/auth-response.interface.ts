8d2a3f359d8a08d7eb27c0f604eecd2b6cc2a48c
moises-herrera <moisesherrera037@gmail.com>
2024-04-13 21:03:35 -0500

    chore: add auth interfaces

---
+++ src/interfaces/auth/auth-response.interface.ts
@@ -0,0 +1,12 @@
+import { IUser } from '..';
+
+/**
+ * The auth response.
+ */
+export interface IAuthResponse {
+  /** Access token. */
+  accessToken: string;
+
+  /** User data. */
+  user: IUser;
+}
---
+++ src/interfaces/auth/auth.interface.interface.ts
@@ -0,0 +1,10 @@
+/**
+ * Represents the authentication credentials.
+ */
+export interface IAuth {
+  /** Email address. */
+  email: string;
+
+  /** Password. */
+  password: string;
+}
