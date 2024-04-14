72c7cd9241b5b75077826464b37f84159d7e2154
moises-herrera <moisesherrera037@gmail.com>
2024-04-14 14:03:25 -0500

    chore: add nodemailer

--- src/config/env-config.ts
+++ src/config/env-config.ts
@@ -1,11 +1,19 @@
+import { DotenvParseOutput } from 'dotenv';
 import { parseEnvConfig } from 'src/utils';
 
 const currentEnvConfig = parseEnvConfig();
 
-const envConfig = {
-  PORT: currentEnvConfig.PORT || 3000,
-  DB_URI: currentEnvConfig.DB_URI as string,
-  JWT_SECRET: currentEnvConfig.JWT_SECRET as string,
+const envConfig: DotenvParseOutput = {
+  PORT: currentEnvConfig.PORT,
+  DB_URI: currentEnvConfig.DB_URI,
+  JWT_SECRET: currentEnvConfig.JWT_SECRET,
+  SMTP_SERVICE: currentEnvConfig.SMTP_SERVICE,
+  SMTP_HOST: currentEnvConfig.SMTP_HOST,
+  SMTP_PORT: currentEnvConfig.SMTP_PORT,
+  SMTP_SECURE: currentEnvConfig.SMTP_SECURE,
+  EMAIL_USER: currentEnvConfig.EMAIL_USER,
+  EMAIL_PASSWORD: currentEnvConfig.EMAIL_PASSWORD,
+  SMTP_SENDER: currentEnvConfig.SMTP_SENDER,
 };
 
 export default envConfig;
