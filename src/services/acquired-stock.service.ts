09b8c18c937e60f997f6431c04cde7d159cc8fd6
moises-herrera <moisesherrera037@gmail.com>
2024-04-18 21:10:30 -0500

    chore: add acquired stock service

---
+++ src/services/acquired-stock.service.ts
@@ -0,0 +1,131 @@
+import { StatusCodes } from 'http-status-codes';
+import { AcquiredStock } from 'src/database/models';
+import {
+  IAcquiredStock,
+  IAcquiredStockDocument,
+  IStandardResponse,
+} from 'src/interfaces';
+import { HttpError } from 'src/utils';
+
+/**
+ * Find all the acquired stocks of a user.
+ *
+ * @returns All the acquired stocks of a user.
+ */
+export const findAll = async (
+  userId: string
+): Promise<IAcquiredStockDocument[]> => {
+  const acquiredStocks = await AcquiredStock.find({
+    user: userId,
+  }).populate('stock');
+
+  return acquiredStocks;
+};
+
+/**
+ * Create a new acquired stock.
+ *
+ * @param stock acquired stock data.
+ * @returns The acquired stock created.
+ */
+export const createOne = async (
+  stock: IAcquiredStock
+): Promise<IStandardResponse<IAcquiredStockDocument>> => {
+  const createdAcquiredStock = await AcquiredStock.create(stock);
+
+  const response: IStandardResponse<IAcquiredStockDocument> = {
+    message: 'La accion fue adquirida correctamente',
+    data: createdAcquiredStock,
+  };
+
+  return response;
+};
+
+/**
+ * Buy a stock.
+ *
+ * @param userId The user id.
+ * @param stockId The stock id.
+ * @param quantity The quantity of the stock.
+ * @returns The acquired stock created or updated.
+ */
+export const buyStock = async (
+  userId: string,
+  stockId: string,
+  quantity: number
+): Promise<IStandardResponse<IAcquiredStockDocument>> => {
+  const acquiredStock = await AcquiredStock.findOne({
+    user: userId,
+    stock: stockId,
+  });
+
+  if (acquiredStock) {
+    acquiredStock.totalQuantity += quantity;
+    await acquiredStock.save();
+  } else {
+    const newAcquiredStock = await AcquiredStock.create({
+      user: userId,
+      stock: stockId,
+      totalQuantity: quantity,
+    });
+
+    return {
+      message: 'La accion fue adquirida correctamente',
+      data: newAcquiredStock,
+    };
+  }
+
+  return {
+    message: 'La accion fue actualizada correctamente',
+    data: acquiredStock,
+  };
+};
+
+/**
+ * Sell a stock.
+ *
+ * @param userId The user id.
+ * @param stockId The stock id.
+ * @param quantity The quantity of the stock.
+ * @returns
+ */
+export const sellStock = async (
+  userId: string,
+  stockId: string,
+  quantity: number
+) => {
+  const acquiredStock = await AcquiredStock.findOne({
+    user: userId,
+    stock: stockId,
+  });
+
+  if (!acquiredStock) {
+    throw new HttpError(
+      'El usuario no tiene acciones de esta empresa',
+      StatusCodes.BAD_REQUEST
+    );
+  }
+
+  if (acquiredStock.totalQuantity < quantity) {
+    throw new HttpError(
+      'El usuario no tiene suficientes acciones de esta empresa',
+      StatusCodes.BAD_REQUEST
+    );
+  }
+
+  acquiredStock.totalQuantity -= quantity;
+
+  if (acquiredStock.totalQuantity === 0) {
+    await acquiredStock.deleteOne();
+
+    return {
+      message: 'La accion fue vendida correctamente',
+    };
+  }
+
+  await acquiredStock.save();
+
+  return {
+    message: 'La accion fue vendida correctamente',
+  };
+};
