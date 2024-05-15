8c124d635fe269c4070acd3471157047e4415465
moises-herrera <moisesherrera037@gmail.com>
2024-05-15 11:54:06 -0500

    refactor: seed database

--- src/database/seed/seed-database.ts
+++ src/database/seed/seed-database.ts
@@ -3,20 +3,34 @@ import { dbConnect } from 'src/database';
 import { Broker, Country, Currency, Stock } from 'src/database/models';
 import { currencies, countries, brokers, stocks } from 'src/database/seed/data';
 
-(async () => {
+const deleteAllRecords = async (): Promise<void> => {
   try {
-    await dbConnect();
-
     await Currency.deleteMany({});
     await Country.deleteMany({});
     await Stock.deleteMany({});
     await Broker.deleteMany({});
+  } catch (error) {
+    console.log('Error while deleting records', error);
+  }
+};
 
+const insertAllRecords = async (): Promise<void> => {
+  try {
     await Currency.insertMany(currencies);
     await Country.insertMany(countries);
     await Stock.insertMany(stocks);
     await Broker.insertMany(brokers);
+  } catch (error) {
+    console.log('Error while inserting records', error);
+  }
+};
 
+(async () => {
+  try {
+    await dbConnect();
+
+    await deleteAllRecords();
+    await insertAllRecords();
     connection.close();
     console.log('Database seeded successfully');
   } catch (error) {
