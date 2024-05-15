c9f45c2a947c6af6fc6663251aa8d4b29f647903
moises-herrera <moisesherrera037@gmail.com>
2024-05-15 11:53:55 -0500

    refactor: update stock seed data

--- src/database/seed/data/stocks.ts
+++ src/database/seed/data/stocks.ts
@@ -1,5 +1,6 @@
 import { Types } from 'mongoose';
 import { IStock } from 'src/interfaces';
+import { currenciesMapped } from '.';
 
 interface ISeedStock extends IStock {
   _id: Types.ObjectId;
@@ -10,85 +11,99 @@ export const stocks: ISeedStock[] = [
     _id: new Types.ObjectId(),
     label: 'Apple Inc.',
     symbol: 'AAPL',
-    price: 146.92,
+    price: 190.92,
+    currency: currenciesMapped['USD'],
   },
   {
     _id: new Types.ObjectId(),
     label: 'Microsoft Corporation',
     symbol: 'MSFT',
-    price: 277.01,
+    price: 421.01,
+    currency: currenciesMapped['USD'],
   },
   {
     _id: new Types.ObjectId(),
     label: 'Amazon.com Inc.',
     symbol: 'AMZN',
-    price: 3372.01,
+    price: 184.01,
+    currency: currenciesMapped['USD'],
   },
   {
     _id: new Types.ObjectId(),
     label: 'Meta Platforms Inc.',
     symbol: 'META',
     price: 332.88,
+    currency: currenciesMapped['USD'],
   },
   {
     _id: new Types.ObjectId(),
     label: 'Alphabet Inc.',
     symbol: 'GOOGL',
-    price: 2728.81,
+    price: 172.81,
+    currency: currenciesMapped['USD'],
   },
   {
     _id: new Types.ObjectId(),
     label: 'Tesla Inc.',
     symbol: 'TSLA',
-    price: 709.67,
+    price: 175.67,
+    currency: currenciesMapped['USD'],
   },
   {
     _id: new Types.ObjectId(),
     label: 'NVIDIA Corporation',
     symbol: 'NVDA',
-    price: 207.5,
+    price: 945.5,
+    currency: currenciesMapped['USD'],
   },
   {
     _id: new Types.ObjectId(),
     label: 'PayPal Holdings Inc.',
     symbol: 'PYPL',
     price: 283.0,
+    currency: currenciesMapped['USD'],
   },
   {
     _id: new Types.ObjectId(),
     label: 'Adobe Inc.',
     symbol: 'ADBE',
-    price: 630.0,
+    price: 486.0,
+    currency: currenciesMapped['USD'],
   },
   {
     _id: new Types.ObjectId(),
     label: 'Netflix Inc.',
     symbol: 'NFLX',
     price: 513.99,
+    currency: currenciesMapped['USD'],
   },
   {
     _id: new Types.ObjectId(),
     label: 'Salesforce.com Inc.',
     symbol: 'CRM',
-    price: 254.0,
+    price: 285.0,
+    currency: currenciesMapped['USD'],
   },
   {
     _id: new Types.ObjectId(),
     label: 'Shopify Inc.',
     symbol: 'SHOP',
-    price: 1545.0,
+    price: 58.0,
+    currency: currenciesMapped['USD'],
   },
   {
     _id: new Types.ObjectId(),
     label: 'Zoom Video Communications Inc.',
     symbol: 'ZM',
-    price: 292.0,
+    price: 63.0,
+    currency: currenciesMapped['USD'],
   },
   {
     _id: new Types.ObjectId(),
     label: 'Spotify Technology S.A.',
     symbol: 'SPOT',
-    price: 232.0,
+    price: 299.0,
+    currency: currenciesMapped['USD'],
   },
 ];
 
